761393a160d44e128339d13c784de715d8de9d5c
empzhang22 <epz6@cornell.edu>
2023-12-19 20:52:56 -0500

    Added channel parsing, created Andrew training corpus with 16000 lines

--- pinghandler.py
+++ pinghandler.py
@@ -1,4 +1,6 @@
 debug = True
+andrew_debug = False
+andrew_debug2 = False
 
 import os
 
@@ -7,13 +9,37 @@ from dotenv import load_dotenv
 load_dotenv()
 TOKEN = os.getenv('DISCORD_TOKEN')
 GUILD = os.getenv('DISCORD_GUILD')
+ANDREW = int(os.getenv('ANDREW_ID'))
 
-def ping_handler(client, message):
+async def ping_handler(client, message):
     if debug: print("in ping_handler")
     if "extract_message" in message.content:
-        andrew_message_extractor(client)
+        await andrew_message_extractor(client)
 
-def andrew_message_extractor(client):
+# Performed testing to arrive at Andrew's user ID
+async def andrew_message_extractor(client):
+    file = open('andrew_text.txt', 'w', encoding="utf-8")
+    if debug: print("in andrew_message_extractor")
     guild = discord.utils.get(client.guilds, name=GUILD)
-    # for channel in guild.channels:
-    #     for 
+    for channel in guild.channels:
+        if andrew_debug: 
+            print("*****CHANNEL NAME: " + channel.name + "*****")
+            print(type(channel))
+        if isinstance(channel, discord.channel.TextChannel):
+            # channel.history uses an asyncrhonous iterator so everything must use async and await
+            await andrew_message_extractor_helper(file, channel)
+    file.close()
+    if debug: print("finished")
+    
+async def andrew_message_extractor_helper(file, channel):
+    if debug: print("in andrew_message_extractor_helper")
+    async for message in channel.history(limit=10000):
+        if andrew_debug:
+            print(message.author.id)
+            #print(message.content)
+        if message.author.id == ANDREW: 
+            if andrew_debug2: print(message.content)
+            file.write(message.content + "\n")
+    if andrew_debug: print('-----------------------------')
+
+    # Writing into a txt file using Python: https://blog.enterprisedna.co/python-write-to-file/
---
+++ writetest.py
@@ -0,0 +1,13 @@
+# tutorial: https://blog.enterprisedna.co/python-write-to-file/
+messages = ["bozo", "rip", "goofy", "ahh", "yapper"]
+
+# Method 1: Write using a list of strings
+# file = open('test.txt', 'w')
+# file.writelines(messages)
+# file.close()
+
+# Method 2: Write individual messages
+# file = open('test.txt', 'w')
+# for message in messages:
+#     file.write(message + '\n')
+# file.close()
